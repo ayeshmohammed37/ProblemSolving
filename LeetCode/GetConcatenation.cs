385bf47c62c4589a48929c435fbcf7c02758ce28
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-08-03 17:56:10 +0300

    LeetCode/GetConcatenation: One Pass Solution

--- LeetCode/GetConcatenation.cs
+++ LeetCode/GetConcatenation.cs
@@ -7,9 +7,10 @@ public partial class Solution
         int n = nums.Length;
         int[] ans = new int[n * 2];
 
-        for (int i = 0; i < n * 2; i++)
+        for (int i = 0; i < n; i++)
         {
-            ans[i] = i >= n ? nums[i - n] : nums[i];
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
         return ans;
     }
